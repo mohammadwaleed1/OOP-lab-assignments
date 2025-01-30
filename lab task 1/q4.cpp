63f76b454a2c69667a69520c4c38f81a0230deb6
mohammadwaleed1 <waleedmohammad484@gmail.com>
2025-01-30 20:37:49 +0500

    Add files via upload

---
+++ lab task 1/q4.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+float kg;
+float pnd;
+cout<<"Enter weight in kg: ";
+cin>>kg;
+pnd= kg* 2.2;
+printf("weight in pounds: %.1f\n",pnd);
+printf("weight in kg: %.1f",kg);
+
+
+return 0;
+}
